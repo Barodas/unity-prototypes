f1451a47b7aecb146a3e62f001e9ab55aa5ce511
Luke Savage <lsava22@hotmail.com>
2025-08-11 08:01:57 +1000

    Idle Combat: Set up project and create sprites
    - Added Character and TargetDummy Sprites
    - Created AbilityData and CharacterData ScriptableObjects

---
+++ idle-combat/Assets/Scripts/AbilityData.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public enum EnergyType
+{
+    Martial = 0,
+    Fire = 1,
+    Water = 2,
+    Earth = 3,
+    Air = 4
+}
+
+[CreateAssetMenu(fileName = "AbilityData", menuName = "Scriptable Objects/AbilityData")]
+public class AbilityData : ScriptableObject
+{
+    public EnergyType energyType = EnergyType.Martial;
+    public int energyCost = 1;
+
+    public EnergyType generateType = EnergyType.Martial;
+    public int generateAmount = 0;
+
+    public int damage = 10;
+}
---
+++ idle-combat/Assets/Scripts/CharacterData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "Scriptable Objects/CharacterData")]
+public class CharacterData : ScriptableObject
+{
+    public int energySlots = 5;
+    public int abilitySlots = 3;
+    public int maxHealth = 100;
+    public int curHealth = 100;
+}
